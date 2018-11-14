71d648b87148d071aef631bfb92d02abf0356026
ezezeta <ezequielm.zarza@gmail.com>
2018-11-14 18:37:47 -0300

    Emprolije un comentario

--- tp-rust/examples/Eze/mutabilidad/mut-04.rs
+++ tp-rust/examples/Eze/mutabilidad/mut-04.rs
@@ -15,17 +15,5 @@ fn main(){
 }
 
 /*
-Cuando	llamamos	a	 	clone()	 ,	el	 	Arc<T>	 	necesita	actualizar	el	contador	de	referencias.	A
-pesar	de	que	no	hemos	usado	ningún	 	mut	 	aquí,	 	x	 	es	un	enlace	inmutable,	tampoco
-tomamos	 	&mut	5	 	o	alguna	mas.	Entonces,	que	esta	pasando?
-Para	entender	esto,	debemos	volver	al	núcleo	de	la	filosofía	que	guía	a	Rust,	seguridad	en
-el	manejo	de	memoria,	y	el	mecanismo	a	través	del	cual	Rust	la	garantiza,	el	sistema	de
-pertenencia,	y	mas	específicamente,	el	préstamo:
-Puedes	tener	uno	u	otro	de	estos	dos	tipos	de	prestamo,	pero	no	los	dos	al	mismo
-tiempo:
-una	o	mas	referencias	( 	&T	 )	a	un	recurso,
-exactamente	una	referencia	mutable	( 	&mut	T	 ).
-Entonces,	esa	es	la	definición	real	de	‘inmutabilidad’:	es	seguro	tener	dos	apuntadores?	En
-el	caso	de	 	Arc<T>	 ’s,	si:	la	mutación	esta	completamente	contenida	dentro	de	la	estructura
-en	si	misma.	No	esta	disponible	al	usuario.	Por	esta	razón,	retorna	 	clone()	 	 	&T	 s.	Si
-proporcionase	 	&mut	T	 s,	seria	un	problema.*/
+Cuando se llama a clone() , el Arc<T> necesita actualizar el contador de	referencias. A pesar de	que no se uso mut aca, x es un enlace inmutable,	tampoco se toma	&mut 5 o alguna	mas. Entonces, que esta pasando? Para entender esto, debemos	volver	al núcleo de la filosofía que guíaaRust, seguridad en el  manejo  de  memoria, y el  mecanismo a   través	del	cual	Rust la  garantiza,  el  sistema	de pertenencia,y mas específicamente, el préstamo: Puedes tener uno u otro de  estos dos tipos de  prestamo, pero no  los dos al  mismo
+tiempo: una o mas referencias (&T )  a   un  recurso, exactamente una referencia  mutable	(&mut T). Entonces, esa es  la  definición  real	de  ‘inmutabilidad’: es  seguro	tener dos apuntadores? En el  caso	de  Arc<T>	 ’s,	si: la  mutación esta completamente	contenida dentro	de  la  estructura en  si  misma.	No esta disponible al usuario.Por esta razón,	retorna	clone()	&T s.Si proporcionase	&mut Ts, seria un problema.*/
